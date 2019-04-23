26d0a362d44a6069c9a4ba910f221b9c28df9905
Jose Antonio Sanchez <jasancheg@gmail.com>
2019-04-23 09:11:40 -0600

    babel temp fix

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,21 @@
-module.exports = function(api) {
+/* eslint-disable prettier/prettier */
+/* eslint-disable flowtype/require-parameter-type */
+/* eslint-disable flowtype/require-return-type */
+/* eslint-disable flowtype/require-valid-file-annotation */
+/**
+ * ./babel.config.js
+ */
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    env: {
-      "development": {
-        "plugins": [
-          "transform-react-jsx-source"
-        ]
-      }
-    }
+    presets: ["babel-preset-expo"]
+    // env: {
+    //   "development": {
+    //     "plugins": [
+    //       "transform-react-jsx-source"
+    //     ]
+    //   }
+    // }
   };
 };
